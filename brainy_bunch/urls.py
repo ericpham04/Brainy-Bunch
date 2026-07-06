4556a08b5d05ee6abf3014f2249b31464254c272
Psalm Williams <psalmwilliams@Psalms-MacBook-Air.local>
2026-07-06 14:10:45 -0400

    Add registration page and routing

--- brainy_bunch/urls.py
+++ brainy_bunch/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home, login_view
+from .views import home, login_view, register
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
     path("login/", login_view, name="login"),
+    path("register/", register, name="register"),
 ]
--- brainy_bunch/views.py
+++ brainy_bunch/views.py
@@ -5,3 +5,6 @@ def home(request):
 
 def login_view(request):
     return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
